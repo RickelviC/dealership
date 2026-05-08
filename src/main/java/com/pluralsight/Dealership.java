7fe3df659eba62d83c6e15f625ca22fa8f19fc40
RickelviC <Cruzrickelvi@gmail.com>
2026-05-08 14:55:37 -0400

    added dealership.csv holds the data we will be using later on/ Dealership.java holds the array of vehicles and other methods we will be working on later on/ Vehicle.java class is how we make new vehicles//// testing class will be used later on to make sure everything is working

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,81 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private List<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * List<Vehicle> calls the Vehicle class to return an array out of each given data
+     */
+
+    public List<Vehicle> getVehiclesByPrice(double minPrice, double maxPrice) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int minYear, int maxYear) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int minMileage, double maxMileage) {
+        return null;
+    }
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+    public void removeVehicle(Vehicle vehicle) {
+        inventory.remove(vehicle);
+    }
+}
---
+++ src/test/java/com/pluralsight/DealershipTest.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealershipTest {
+
+
+/*    @Test
+    public void getVehiclesByPrice_getVehiclesByPriceIsNull_getVehiclesByPriceReturnsNull() {
+        //Arrange
+        Dealership dealership = new Dealership("carshop", "around the block", "123-456-7890");
+
+        //Act
+        List actual = dealership.getVehiclesByPrice(0, 10);
+
+        //Assert
+        assertNull(actual);
+    }*/
+
+}
